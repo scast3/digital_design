38db3a9c7b859cc6171223feb7e306058c78f52e
scast_lx <santiagoc333@gmail.com>
2025-09-04 19:28:05 -0600

    complete hw4

--- hw4/hw4.vhd
+++ hw4/hw4.vhd
@@ -1,6 +1,34 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use work.basicBuildingBlocks.ALL;
+--use work.basicBuildingBlocks.ALL;
+
+entity genericAdder is
+    generic(N: integer := 4);
+    port( a,b: in std_logic_vector(N-1 downto 0);
+        sum: out std_logic_vector(N-1 downto 0));
+end genericAdder;
+----------------------------------------------
+entity genericCompare is
+    generic(N: integer := 4);
+    port( x,y : in std_logic_vector(N-1 downto 0);
+        g,l,e: out std_logic);
+end genericCompare;
+----------------------------------------------
+entity genericMux2x1 is
+    generic(N: integer := 4);
+    port( y1,y0: in std_logic_vector(N-1 downto 0);
+        s: in std_logic;
+        f: out std_logic_vector(N-1 downto 0) );
+end genericMux2x1;
+----------------------------------------------
+entity genericRegister is
+    generic(N: integer := 4);
+    port( clk, reset,load: in std_logic;
+        d: in std_logic_vector(N-1 downto 0);
+        q: out std_logic_vector(N-1 downto 0) );
+end genericRegister;
+----------------------------------------------
+
 entity mod10Counter is
     Port ( clk : in STD_LOGIC;
         reset : in STD_LOGIC;
@@ -37,22 +65,22 @@ architecture Behavioral of mod10Counter is
             q: out std_logic_vector(N-1 downto 0) );
     end component;
 
-signal countPlusOne : STD_LOGIC_VECTOR(3 downto 0);
-signal nextCount : STD_LOGIC_VECTOR(3 downto 0);
-signal currentCount : STD_LOGIC_VECTOR(3 downto 0);
-signal inputCtrlVec: STD_LOGIC_VECTOR(2 downto 0);
-signal outputCtrlVec: STD_LOGIC_VECTOR(2 downto 0);
-signal equalNine, muxSel, regEnb: STD_LOGIC;
+    signal countPlusOne : STD_LOGIC_VECTOR(3 downto 0);
+    signal nextCount : STD_LOGIC_VECTOR(3 downto 0);
+    signal currentCount : STD_LOGIC_VECTOR(3 downto 0);
+    signal inputCtrlVec: STD_LOGIC_VECTOR(2 downto 0);
+    signal outputCtrlVec: STD_LOGIC_VECTOR(2 downto 0);
+    signal equalNine, muxSel, regEnb: STD_LOGIC;
 
 begin
     add_inst : genericAdder
         GENERIC MAP(4)
-        PORT MAP(a => ________, b => _________, sum => _________);
+        PORT MAP(a => currentCount, b => "0001", sum => countPlusOne);
 
     comp_inst : genericCompare
         GENERIC MAP(4)
-        PORT MAP(x => ________, y => _____, g => open, l => open,
-        e => ________);
+        PORT MAP(x => currentCount, y => "1001", g => open, l => open,
+        e => equalNine);
 
     mux_inst: genericMux2x1
         GENERIC MAP(4)
